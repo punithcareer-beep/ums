bab73b43d84817948c5cab01ef310a0f742ffd5f
Andluru Sai Punith Reddy <punithreddyforstudy@gmail.com>
2026-06-07 02:40:41 +0530

    Add GitHub Action to build Android APK

---
+++ android/app/src/main/java/com/ksrmce/ums/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ksrmce.ums;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ksrmce.ums',
+  appName: 'KSRMMS',
+  webDir: 'out'
+};
+
+export default config;
